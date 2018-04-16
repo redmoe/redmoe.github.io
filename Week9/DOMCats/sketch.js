0de9b6f720056541f10108ec2410cd5657c4f437
redmoe <31392444+redmoe@users.noreply.github.com>
2018-04-16 11:14:57 -0400

    fixe

--- Week9/DOMCats/sketch.js
+++ Week9/DOMCats/sketch.js
@@ -1,22 +1,32 @@
 var data = [];
 var buttonElement;
 var selectedDay;
-var selectedMonth;
-var text;
+// var selectedMonth;
+ var text;
 function onButtonPressed() {
-	loadJSON('https://api.abalin.net/get/namedays?day=' + selectedDay + '&month=' + selectedMonth,newDay);
+	loadJSON('https://anapioficeandfire.com/api/characters/' + int(random(0,460)), newDay);
 }
 function setup () {
 	noCanvas();
-	createCanvas(600,600);
-	var doc = document.body;
+	// createCanvas(600,600);
+	// var doc = document.body;
 
     loadJSON('https://anapioficeandfire.com/api/characters/453', newDay);
-    buttonElement = createButton('names of today');
+    buttonElement = createButton('for proper Game of Thrones spelling maybe');
     buttonElement.mousePressed(onButtonPressed);
-    text = createElement('h1', 'The names of today are:');
+   // text = createElement('h1', "HEY");
 }
 function newDay (data) {
-	text = data.name[0];
+	//var t = document.createTextNode(data.name);
+	//text.appendChild(t);
+		// console.log(text.innerHTML);
+		// console.log(text);
+
+	//console.log(data[0].name);
+	 text.innerHTML = data.name;
+	  createElement('h1', "I'm pretty sure their name is spelled " + text.innerHTML + ".");
+
+	 console.log(text);
+	  //text = 
 }
 // https://anapioficeandfire.com/api/characters/453
