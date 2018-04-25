815f6b157c091e2e714332278e9ca5e5d648679f
redmoe <31392444+redmoe@users.noreply.github.com>
2018-04-24 21:26:08 -0400

    Cloning aframe

--- WebVR/AframeVR/sketch.js
+++ WebVR/AframeVR/sketch.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('bus', {
   init: function () {
     var targetEl = this.el;  
     //targetEl.setAttribute('color', 'blue');
-    targetEl.setAttribute('scale','1.5f 1.5f 1.5f')
+    //targetEl.setAttribute('scale','1.5f 1.5f 1.5f')
     console.log(targetEl);
  //    targetEl.addEventListener('click', function () {
  //    	targetEl.setAttribute('dynamic-body',"mass:100");
@@ -10,3 +10,30 @@ AFRAME.registerComponent('bus', {
 
   }
 });
+var num =-1;
+var zOffset = 0;
+for (var i = 0; i < 15; i++) {
+	  //  console.log("Hey all");
+    //console.log(i);
+
+  AFRAME.registerComponent('bus'+i, {
+  init: function () {
+  	num++;
+    var targetEl = this.el; 
+    if (num > 4) {
+    	num = 0;
+    	zOffset += 4;
+
+    } 
+    //targetEl.setAttribute('color', 'blue');
+    targetEl.object3D.position.set(num*2.3,-2,zOffset);
+        //targetEl.object3D.scale.set(1,1,1);
+
+     console.log(num);
+ //    targetEl.addEventListener('click', function () {
+ //    	targetEl.setAttribute('dynamic-body',"mass:100");
+	// });
+
+  }
+});
+}
