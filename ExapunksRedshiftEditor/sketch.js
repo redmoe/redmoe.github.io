ce1527447bd82cf5970e3927de893e40ca20d9ad
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2018-08-11 18:43:59 -0400

    data

---
+++ ExapunksRedshiftEditor/sketch.js
@@ -0,0 +1,47 @@
+var boardSize = 10;
+var blockSize = 50;
+var arrayPos = [];
+function mouseClicked() {
+	var mouseGridPos = createVector(int(mouseX/blockSize), int(mouseY/blockSize) );
+	collisionDetection(mouseGridPos);
+	logToData ();
+}
+
+function collisionDetection(newPos) {
+	if (newPos.x >= 0 && newPos.x < boardSize && newPos.y >= 0 && newPos.y < boardSize) {
+		var ind = (newPos.y*10 + newPos.x);
+		if (arrayPos[ind] == 1) {
+			arrayPos[ind] = 0;
+		}
+		else {
+			arrayPos[ind] = 1;
+		}
+	}
+}	
+function setup() {
+	createCanvas(boardSize*blockSize,boardSize*blockSize);
+	for(var i = 0; i < boardSize*boardSize; i++) {
+    	arrayPos.push(0);
+	}
+}
+function draw () {
+	noStroke();
+	background(0);
+	for (var i = 0; i < arrayPos.length; i++) {
+		if (arrayPos[i] == 1) {
+			fill('#ffffff');
+			rect(Math.floor(i%10)*blockSize,Math.floor((i/10)%10)*blockSize,blockSize,blockSize);
+		}
+	}
+}
+
+function logToData () {
+	var dataString = "";
+	for (var x = 0; x < boardSize; x++) {
+		dataString += "\nDATA";
+		for (var y = 0; y < boardSize; y++) {
+			dataString += " " + arrayPos[x*10+y];
+		}
+	}
+	console.log(dataString);
+}
