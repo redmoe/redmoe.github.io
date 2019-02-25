6103c7ebcc33a5baf806db5e76ad8d5a306c8c3d
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2019-02-25 17:34:14 -0500

    w

--- Computer/botnets/main.js
+++ Computer/botnets/main.js
@@ -30,8 +30,6 @@
 // 		viruses:[],
 // 		currentImage:0,
 // 		cash:10,
-
-
 // 		// product:'gloves',
 // 		// image:'glove.png',
 // 		// link:'https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd/related?hl=en',
@@ -67,6 +65,26 @@
 
 
 
+// function draw() {
+// 	console.log('draw');
+// }
+
+// function loop(timestamp) {
+//   var progress = timestamp - lastRender
+
+//   main.update(progress)
+// //  draw()
+
+//   lastRender = timestamp
+//   window.requestAnimationFrame(loop);
+// }
+
+// var lastRender = 0;
+// window.requestAnimationFrame(loop);
+
+
+
+
 Vue.component('computer', {
 	props: {
 		name: {
@@ -74,74 +92,80 @@ Vue.component('computer', {
 		},
 	},
 	data() {
+		main.tots.push(this);
 		return {
 			ip:String(Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255) + 0),
-			isHacked:false
+			isHacked:false,
+			ram:(Math.floor(Math.random()*15)+1)*8,
+			mem:(Math.floor(Math.random()*15)+1)*8,
+			hackTime:0,
 		}
 	},
 	methods:{
 		HackClick() {
-			this.$emit('hackr-emit',this);
+		//	this.$emit('hackr-emit',this);
 			main.hacking(this);
-			console.log("Hack step 1");
 			this.isHacked=true;
-			
 		},
-		RandomMethod() {
-			console.log("blarg");
+		IgnoreClick() {
+			main.ignore(this);
+		},
+		OverloadClick() {
+			main.overload(this);
 		}
-	},
+	},//v-if:[hackTime>100]=HackClick'		
 	template:
-	`<div :hidden="isHacked">
+	`<div v-show:v-if="hackTime<100">
 		{{ip}}
 		<button @click='HackClick'>
 			Begin Hacking!
 		</button>
-	</div>`
-
-
+		<button @click='IgnoreClick'>Ignore!</button>
+		<progress :value=hackTime max=100></progress>
+	</div>`,
+	computed: {
+		telemetry() {
+			return `${this.ip} / ${this.isHacked} / ${this.ram} / ${this.mem}`;
+		}
+	}	
 }) 
+		// <progress :value=progress max=100></progress>
 
 var main = new Vue({ 
 	el: '#app', 
 	data: {
 		bots:[],
 		nots:[],
-		//name:'My Computer',
+		tots:[],
 		stat:{
 			ram:200,
 			mem:200,
 			ip:127.001
 		},
-		images:[
+		upgrades:[
 			{
-				id:"memory",
-				img:"noun_Memory_1254336.png"
-			},
-			{
+				name:"RAM",				
 				id:"ram",
-				img:"noun_ram_1760674.png"
-			}
-		],
-		shop:[
-			{
-				name:"RAM",
+				img:"noun_ram_1760674.png",
 				cost:2,
 			},
 			{
 				name:"Memory",
-				cost:1,
-			}			
-		],
+				id:"mem",
+				img:"noun_Memory_1254336.png",
+				cost:1
+			}
+		],		
 		currentImage:0,
 		cash:10,
-
+		searching:false,
 	},
 	methods:{
 		hacking(arg) {
 			this.bots.push(arg);
-			this.nots.splice( this.nots.indexOf(arg),1);
+			this.nots.splice( this.nots.indexOf(arg),0);
 			console.log(arg.ip);
+			this.searching=false;
 		},
 		Upgrade(newItem, index) {
 			this.stat[Object.keys(this.stat)[index]]++;
@@ -151,23 +175,53 @@ var main = new Vue({
 			this.currentImage=newImg;
 		},
 		Search () {
+			this.searching=true;
 			// const vueContainer = document.createElement('div')
 			// document.getElementById('app').appendChild(vueContainer)
 			// vm = ( new ( Vue.extend(main) ) ).$mount(vueContainer)
-
-		//	var arc = new computer()
-		//	document.createElement("computer");
-		///sdocument.getElementById('app').appendChild(document.createTextNode("Water");)
+			//	var arc = new computer()
+			//	document.createElement("computer");
+			///sdocument.getElementById('app').appendChild(document.createTextNode("Water");)
 		 	this.nots.push(0);
-		}	
+		},
+		ignore (ele) {
+			this.nots.splice( this.nots.indexOf(ele),1);
+			console.log("ignored");
+			this.searching=false;
+		},
+		update() {
+		//	setTimeout(this.update, this.bots.length*64);
+			//console.log('update');
+			this.cash+=this.bots.length;
+			this.tots.forEach(function(element) {
+		   		element.hackTime++;
+		   		console.log(element.progress);
+		    });
+		    console.log(this.nots[0]);
+
+			// for (this.bots.length)
+			// this.bots.length+=this.bots.length/64
+		}
+
 	},
 	computed: {
 		title() {
-			return this.name + ' ' + this.stat.ip;
+			return 'C&C ' + this.stat.ip;
 		},
 		image () {
-			return this.images[this.currentImage].img;
+			return this.upgrades[this.currentImage].img;
+		},
+		totalPower() {
+		   let totalRam = 0;
+		   let totalMem = 0;
+
+		    this.bots.forEach(function(element) {
+		   		totalRam+=element.ram;
+		   		totalMem+=element.mem;
+		    });
+			return `${this.bots.length} / ${totalRam} / ${totalMem}`;
 		}
 	}	
-})
+}) 
 
+setInterval(main.update, 128);
